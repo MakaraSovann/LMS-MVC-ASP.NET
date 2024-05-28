b1957412f24ec5cd1c750a0335e4e93b318acfd0
MakaraSovann <113426838+MakaraSovann@users.noreply.github.com>
2024-05-28 19:21:06 +0700

    commit
    update database

--- LMSMVC/obj/Debug/net8.0/LMSMVC.AssemblyInfo.cs
+++ LMSMVC/obj/Debug/net8.0/LMSMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMSMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e60c57a811696033a3c85e0504d1077f24c41ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMSMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMSMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
